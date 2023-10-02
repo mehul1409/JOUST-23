b4b96e3cc42570b31d8a7c6ddc68c38c7aa4602c
mehul1409 <mehulbansalswm1234@gmail.com>
2023-10-03 03:18:45 +0530

    adding the event section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Homepage from "./components/Homepage/homepage.jsx"
 import Competitions from './components/Competitions/Competitions.jsx'
 import Members from './components/Members/Members.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import Events from './components/Events/Events.jsx'
 
 const App = () => {
   return (
     <div>
       <Homepage />
       <Competitions />
+      <Events />
       <Members />
       <Footer />
     </div>
---
+++ src/components/Events/Events.jsx
@@ -0,0 +1,60 @@
+import "./Events.css";
+
+const Events = () => {
+    return (
+        <>
+            <div className="eventsection">
+                <div className="eventheadingbox">
+                    <div className="eventbgheading">EVENTS</div>
+                    <div className="eventheading">OUR EVENTS</div>
+                </div>
+                <div className="eventdetails">It is an educational simulation and extracurricular activity in which students typically role-play as delegates to the United Nations and simulate UN committees and other international organizations. The goal of Model United Nations is to provide students with an opportunity to learn about diplomacy, international relations, and the workings of the United Nations while also developing skills in public speaking, negotiation, research, and critical thinking.</div>
+                <div className="eventboxsection">
+                    <div className="eventbox">
+                        <div className="left">
+                            <img src="./images/1.jpg" alt="" />
+                        </div>
+                        <div className="right">
+                            <div className="eventname">UNITED NATIONS HUMAN RIGHTS COUNCIL</div>
+                            <div className="eventcontent">The UNHRC, or the United Nations Human Rights Council, is a prominent intergovernmental body within the United Nations system responsible for promoting and protecting human rights around the world. Established in March 2006, the UNHRC replaced the previous United Nations Commission on Human Rights, aiming to address several shortcomings of its predecessor. The primary mission of the UNHRC is to strengthen the promotion and protection of human rights globally. It works towards this goal by engaging in various activities</div>
+                        </div>
+                    </div>
+                    <div className="eventbox alternateeventbox">
+                        <div className="right">
+                            <div className="eventname">ALL INDIA POLITICAL PARTY MEET</div>
+                            <div className="eventcontent">All India Political Party Meet is a non-technical but powerful committee. It is a meeting between all the political parties of the nation. Typically called before the session of the Parliament or before the introduction of a bill, this committee aims to arrive at a consensus before the sessions begin. It is a supplementary body to the Parliament without any legislative provisions. The AIPPM Committee, unlike the other conventional MUN committee, is characterized by unrestricted political debate, cross talks, and high levels of negotiations focussing on emulating political realities by bringing to light various layers of polity and governance in India.</div>
+                        </div>
+                        <div className="left">
+                            <img src="./images/7.jpg" alt="" />
+                        </div>
+                    </div>
+                    <div className="eventbox">
+                        <div className="left">
+                            <img src="./images/8.jpg" alt="" />
+                        </div>
+                        <div className="right">
+                            <div className="eventname">UNITED NATIONS ENVIRONMENT PROGRAMME</div>
+                            <div className="eventcontent">
+                            The United Nations Environment Programme, is a specialized agency of the United Nations dedicated to addressing environmental issues and promoting sustainable development worldwide. Established in 1972, UNEP has played a crucial role in advocating for environmental protection, conservation, and sustainable resource management. It serves as a platform for international cooperation and action on a wide range of environmental issues, with the aim of creating a more sustainable and environmentally responsible future for the planet and its inhabitants.
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="eventbox alternateeventbox">
+                        <div className="right">
+                            <div className="eventname">INTERNATIONAL PRESS</div>
+                            <div className="eventcontent">
+                            Instead of the traditional public speaking-focused aspect of Model UN, the reporters of the International Press move from committee to committee, carefully examining delegates’ foreign policy, observing new alliances between nations, and reporting on the key ideas of various resolutions. Additionally, the reporters organize and attend press conferences, interview delegates, and write short articles, resulting in either high praise of innovative and favorable resolution papers, or bring upon heavy criticism of controversial solutions passed by a given committee. They do not represent any country’s interests, allowing them to act as unbiased journalists. Reporters are both active and passive participants in the conference ensuring the transparency and accountability of the United Nations. 
+                            </div>
+                        </div>
+                        <div className="left">
+                            <img src="./images/3.jpg" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Events;
